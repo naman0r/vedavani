c7e43d6bfda3f648cdb87ec9383cd70115792033
Naman Rusia <naman.rusia14@gmail.com>
2025-03-30 21:48:22 -0400

    initial commit of ios app

---
+++ ios-app/ios-appTests/ios_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appTests.swift
+//  ios-appTests
+//
+//  Created by Naman Rusia on 3/30/25.
+//
+
+import Testing
+@testable import ios_app
+
+struct ios_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
